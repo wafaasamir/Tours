938bc0d802dbb0fe515db6b8aa9ee4facbfe4b1a
wafaa samir <wafaasamir1999@gmail.com>
2024-12-15 03:59:39 +0200

    nav and footer and home modified

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,2 +1,2 @@
-fetch("../../../pages/Nav/nav.html").then(response => response.text()).then(data => document.getElementById("nav").innerHTML = data);
-fetch("../../../pages/Footer/footer.html").then(response => response.text()).then(data => document.getElementById("footer").innerHTML = data);
+fetch("../../nav.html").then(response => response.text()).then(data => document.getElementById("nav").innerHTML = data);
+fetch("../../footer.html").then(response => response.text()).then(data => document.getElementById("footer").innerHTML = data);
