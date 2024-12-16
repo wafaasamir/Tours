a96acf34edffe4d87e759f1708bbb252bdd5a8c5
Zeina Seif <zeinaseif760@gmail.com>
2024-12-17 01:00:00 +0200

    dest-component-v1

---
+++ src/pages/Destination.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Card from '../Components/Destination/Card'
+import PlacesSection from '../Components/Destination/PlacesSection'
+import '../styles/destination.css'
+
+function Destination() {
+  return (
+    <>
+      <PlacesSection/>
+    </>
+  )
+}
+
+export default Destination
+
