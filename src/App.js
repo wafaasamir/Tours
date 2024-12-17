985c60535512e50d3bc53fa2ab27dcea4af2bc3e
YasserSoltan <yassersoltan64@gmail.com>
2024-12-17 17:49:22 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,13 @@ import React from "react";
 import MainNav from "./shared/header/MainNav";
 import Footer from "./shared/footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
-// import MainNav from "./shared/header/MainNav";
-// import Footer from "./shared/footer/Footer";
-// import Destination from "./pages/Destination";
-// import Contact from "./pages/Contact";
-// import TourGrid from "./pages/TourGrid";
-// import BlogPage from "./pages/BlogPage";
 
-
-// import AppRoutes from "./routes/AppRoutes";
 function App() {
   return (
     <div className="App">
       <MainNav />
       <AppRoutes />
-      {/* <BlogPage /> */}
-      {/* <Contact /> */}
-      {/* <Destination /> */}
-      {/* <TourGrid />  */}
-      {/* <MainNav/> */}
-      {/* <Footer/> */}
-
+      <Footer />
     </div>
   );
 }
