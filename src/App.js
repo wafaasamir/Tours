4b5e58cef73a3bd3255e4662fc58a84e01c92c86
Nada Abdalla <nadasabdalla644@gmail.com>
2024-12-17 13:20:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import ContactPage from "./pages/ContactPage";
 import DestinationDetails from "./pages/DestinationDetailsPage";
 // import BlogPage from "./pages/BlogPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <ContactPage /> */}
       <DestinationDetails />
       {/* <BlogPage /> */}
     </div>
