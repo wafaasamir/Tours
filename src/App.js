9a5c959a27c0b39f9c32872eb4d71d03a160ccc0
Nada Abdalla <nadasabdalla644@gmail.com>
2024-12-17 03:45:13 +0200

    blog-south-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import ContactPage from "./pages/ContactPage";
+// import ContactPage from "./pages/ContactPage";
+import DestinationDetails from "./pages/DestinationDetailsPage";
+// import BlogPage from "./pages/BlogPage";
+
 function App() {
   return (
     <div className="App">
-      <ContactPage />
+      {/* <ContactPage /> */}
+      <DestinationDetails />
+      {/* <BlogPage /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-
+// import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-      <App />
+    <App />
     {/* </Provider> */}
   </React.StrictMode>
 );
