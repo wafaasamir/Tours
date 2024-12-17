659f147935410cdcf063026febf22aab390a7cbf
Zeina Seif <zeinaseif760@gmail.com>
2024-12-17 18:15:51 +0200

    booking-checkout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-
 import React from "react";
-import AppRoutes from "./routes/AppRoutes";
+// import BookingPage from "./pages/BookingPage";
+// import AppRoutes from "./routes/AppRoutes";
 // import MainNav from "./shared/header/MainNav";
 // import Footer from "./shared/footer/Footer";
 // import Destination from "./pages/Destination";
 // import Contact from "./pages/Contact";
 // import TourGrid from "./pages/TourGrid";
+// import Checkout from "./pages/Checkout";
 
-import AppRoutes from "./routes/AppRoutes";
+// import AppRoutes from "./routes/AppRoutes";
 function App() {
   return (
     <div className="App">
-      <AppRoutes />
+      {/* <AppRoutes /> */}
 
       {/* <Contact /> */}
       {/* <Destination /> */}
       {/* <TourGrid />  */}
       {/* <MainNav/> */}
       {/* <Footer/> */}
-
+      {/* <Checkout/> */}
+      {/* <BookingPage/> */}
     </div>
   );
 }
