84abca0393f39c8b0d90d068c465742f9c10e7f9
YasserSoltan <yassersoltan64@gmail.com>
2024-12-17 14:48:43 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 // import DestinationDetails from "./pages/DestinationDetailsPage";
 import BlogPage from "./pages/BlogPage";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div className="App">
       {/* <DestinationDetails /> */}
-      <BlogPage />
+      <AppRoutes />
     </div>
   );
 }
