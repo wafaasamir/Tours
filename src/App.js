de5a467b6ffc9c622eca38878af6c49a2eed16a6
Zeina Seif <zeinaseif760@gmail.com>
2024-12-17 14:40:22 +0200

    newsletter-component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Newsletter from "./shared/newsletter/Newsletter";
 // import Destination from "./pages/Destination";
 // import Contact from "./pages/Contact";
-import ToursGrid from "./pages/ToursGrid";
+// import ToursGrid from "./pages/ToursGrid";
 function App() {
   return (
     <div className="App">
       {/* <Contact /> */}
       {/* <Destination /> */}
-      <ToursGrid /> 
+      {/* <ToursGrid />  */}
+      <Newsletter/>
     </div>
   );
 }
